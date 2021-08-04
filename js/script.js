f3eb1de9e47ad5b7d3c9ef2e654db6c079788f2c
Morgan_Muller <morgaine.muller@gmail-com>
2021-08-04 21:57:24 +0200

    Adding Madonna and Oprah

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
-let mijnButton = document.querySelector(".button");
+let mijnButton = document.querySelector(".bezos .button");
 let mijnEersteStap = document.querySelector(".bezos");
+let mijnTweedeButton = document.querySelector(".oprah .button");
+let mijnOprahKader= document.querySelector("div .oprah");
+let mijnMadonnaKader = document.querySelector(".madonna");
+let mijnDerdeButton = document.querySelector(".madonna .button");
+
 function eersteStap(){
     mijnEersteStap.classList.add("onzichtbaar")
 }
 mijnButton.addEventListener("click", eersteStap)
+
+function tweedeStap(){
+    mijnTweedeButton.classList.add("onzichtbaar");
+    mijnOprahKader.classList.add("fotoOprah");
+    mijnMadonnaKader.classList.remove("onzichtbaar");
+}
+
+mijnTweedeButton.addEventListener("click", tweedeStap);
+
+function derdeStap(){
+    mijnDerdeButton.classList.add("onzichtbaar");
+    mijnOprahKader.classList.add("fotoMadonna");
+    mijnMadonnaKader.classList.add("onzichtbaar");
+}
+
+mijnDerdeButton.addEventListener("click", derdeStap);
